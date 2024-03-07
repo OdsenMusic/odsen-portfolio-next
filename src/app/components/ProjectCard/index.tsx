1ea47cc8c6be06e1951dd3dee5d5f4d34431ca39
Joel Oliver <jolivermillan@gmail.com>
2024-03-07 11:42:49 +0100

    functional animations

--- src/app/components/ProjectCard/index.tsx
+++ src/app/components/ProjectCard/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 import TechnologyCard from "../TechnologyCard";
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
 import { ProjectCardProps } from "../ProjectsPage/types";
 import { TechnologyCardProps } from "../TechnologyCard/types";
 
@@ -24,60 +24,46 @@ const ProjectCard: React.FC<CombinedProps> = ({
 }) => {
   return (
     <>
-      <AnimatePresence mode="wait">
-        {!isRightContainerOpen && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            className={styles.fullContainer}
-          >
-            <a className={styles.link} href={href}>
-              <div className={styles.mainContainer}>
-                <h1 className={styles.projectTitle}>{name}</h1>
+      <motion.div className={styles.fullContainer}>
+        <a className={styles.link} href={href}>
+          <div className={styles.mainContainer}>
+            <h1 className={styles.projectTitle}>{name}</h1>
 
-                <div className={styles.imgContainer}>
-                  <img className={styles.projectImg} src={img} alt="" />
-                </div>
+            <div className={styles.imgContainer}>
+              <img className={styles.projectImg} src={img} alt="" />
+            </div>
+          </div>
+        </a>
+
+        <AnimatePresence>
+          {!isRightContainerOpen && (
+            <motion.div layout="position" className={styles.textContainer}>
+              <p>
+                {description}
+                <br /> <br />
+              </p>
+              <div className={styles.bottomContainer}>
+                <TechnologyCard
+                  react={react}
+                  javascript={javascript}
+                  html={html}
+                  mongodb={mongodb}
+                  express={express}
+                  css={css}
+                />
+                <button
+                  onClick={() => {
+                    setIsRightContainerOpen(true);
+                  }}
+                  className={styles.openMore}
+                >
+                  +
+                </button>
               </div>
-            </a>
-
-            <>
-              <div className={styles.textContainer}>
-                <p>
-                  {description}
-                  <br /> <br />
-                </p>
-                <div className={styles.bottomContainer}>
-                  <TechnologyCard
-                    react={react}
-                    javascript={javascript}
-                    html={html}
-                    mongodb={mongodb}
-                    express={express}
-                    css={css}
-                  />
-                  <button
-                    onClick={() => {
-                      setIsRightContainerOpen(true);
-                    }}
-                    className={styles.openMore}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-            </>
-          </motion.div>
-        )}{" "}
-      </AnimatePresence>
-
-      {isRightContainerOpen && (
-        <h1 className={styles.projectTitleSmall}>
-          {name} <button className={styles.openMore}> + </button>
-        </h1>
-      )}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </>
   );
 };
--- src/app/components/ProjectsPage/index.tsx
+++ src/app/components/ProjectsPage/index.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import styles from "./styles.module.css";
-import { motion, AnimatePresence, easeInOut, easeOut } from "framer-motion";
+import {
+  motion,
+  AnimatePresence,
+  easeInOut,
+  easeOut,
+  LayoutGroup,
+} from "framer-motion";
 import ProjectCard from "../ProjectCard";
 import React, { useState, FC } from "react";
 import glotonImg from "../../../../public/static/images/glotonImg.gif";
@@ -18,135 +24,146 @@ export const ProjectsPage: FC = () => {
       transition={{ ease: "easeOut", duration: 3 }}
       className={styles.viewport}
     >
-      <motion.div
-        layout
-        transition={{ duration: 0.5, ease: [0.65, 0.05, 0.36, 1] }}
-        initial={{ opacity: 0, translateY: 200 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        className={`${styles.mainContainer} ${
-          isRightContainerOpen && styles.closed
-        }`}
-      >
-        {" "}
-        <ProjectCard
-          img={glotonImg.src}
-          description={`Biggest project so far. Based off of the Glovo website, its a fully
+      <LayoutGroup>
+        <motion.div
+          layout="position"
+          transition={{
+            duration: 0.5,
+            ease: [0.65, 0.05, 0.36, 1],
+          }}
+          initial={{ opacity: 0, translateY: 200 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          className={`${styles.mainContainer} ${
+            isRightContainerOpen && styles.closed
+          }`}
+        >
+          {" "}
+          <ProjectCard
+            img={glotonImg.src}
+            description={`Biggest project so far. Based off of the Glovo website, its a fully
           functional website with login and able to make purchases.`}
-          name={`Gloton`}
-          stack={`          HTML, CSS, JavaScript, React, ExpressJS, MongoDB, JWT, Jest, Cypress
+            name={`Gloton`}
+            stack={`          HTML, CSS, JavaScript, React, ExpressJS, MongoDB, JWT, Jest, Cypress
           `}
-          href={"https://gloton.netlify.app/"}
-          isRightContainerOpen={isRightContainerOpen}
-          setIsRightContainerOpen={setIsRightContainerOpen}
-          react={true}
-          javascript={true}
-          html={true}
-          mongodb={true}
-          css={true}
-          express={true}
-        />
-        <ProjectCard
-          img={taskerImg.src}
-          description={`Note and reminder taking app. Featured with backend, groups, color personalizing and trash bin`}
-          name={`Tasker`}
-          stack={`          HTML, CSS, JavaScript, React, ExpressJS.
+            href={"https://gloton.netlify.app/"}
+            isRightContainerOpen={isRightContainerOpen}
+            setIsRightContainerOpen={setIsRightContainerOpen}
+            react={true}
+            javascript={true}
+            html={true}
+            mongodb={true}
+            css={true}
+            express={true}
+          />
+          <ProjectCard
+            img={taskerImg.src}
+            description={`Note and reminder taking app. Featured with backend, groups, color personalizing and trash bin`}
+            name={`Tasker`}
+            stack={`          HTML, CSS, JavaScript, React, ExpressJS.
           `}
-          href={""}
-          isRightContainerOpen={isRightContainerOpen}
-          setIsRightContainerOpen={setIsRightContainerOpen}
-          react={true}
-          javascript={true}
-          html={true}
-          mongodb={false}
-          css={true}
-          express={true}
-        />
-        <ProjectCard
-          img={calculatorImg.src}
-          description={`My first react project. Basic calculator with an excellent design and history.`}
-          name={`Calculator`}
-          stack={`HTML, CSS, JavaScript, React.
+            href={""}
+            isRightContainerOpen={isRightContainerOpen}
+            setIsRightContainerOpen={setIsRightContainerOpen}
+            react={true}
+            javascript={true}
+            html={true}
+            mongodb={false}
+            css={true}
+            express={true}
+          />
+          <ProjectCard
+            img={calculatorImg.src}
+            description={`My first react project. Basic calculator with an excellent design and history.`}
+            name={`Calculator`}
+            stack={`HTML, CSS, JavaScript, React.
             `}
-          href="https://odsencalculator.netlify.app/"
-          isRightContainerOpen={isRightContainerOpen}
-          setIsRightContainerOpen={setIsRightContainerOpen}
-          react={true}
-          javascript={true}
-          html={true}
-          mongodb={false}
-          css={true}
-          express={false}
-        />
-      </motion.div>
-      <AnimatePresence mode="popLayout">
-        {isRightContainerOpen && (
-          <motion.div
-            initial={{ opacity: 0, translateY: 200 }}
-            animate={{ opacity: 1, translateY: 0 }}
-            exit={{ opacity: 0, translateY: 200 }}
-            transition={{ ease: "easeOut", duration: 0.6, delay: 0.5 }}
-            className={styles.expandedContainer}
-          >
-            <div className={styles.topBar}></div>
+            href="https://odsencalculator.netlify.app/"
+            isRightContainerOpen={isRightContainerOpen}
+            setIsRightContainerOpen={setIsRightContainerOpen}
+            react={true}
+            javascript={true}
+            html={true}
+            mongodb={false}
+            css={true}
+            express={false}
+          />
+          L
+        </motion.div>
+        <AnimatePresence mode="popLayout">
+          {isRightContainerOpen && (
+            <motion.div
+              initial={{ opacity: 0, translateY: 200 }}
+              animate={{ opacity: 1, translateY: 0 }}
+              exit={{ opacity: 0, translateY: 200 }}
+              transition={{
+                ease: "easeOut",
+                duration: 0.6,
 
-            <button
-              className={styles.closeButton}
-              onClick={() => {
-                setIsRightContainerOpen(false);
+                exit: { delay: 0, duration: 0.1 },
               }}
+              className={styles.expandedContainer}
             >
-              X
-            </button>
-            <div className={styles.textContainer}>
-              <h1>Gloton</h1>
-              <p>
-                Gloton represents the pinnacle of my project experiences,
-                inspired by the innovative Glovo platform to deliver a
-                state-of-the-art, fully functional website. This project
-                showcases a user-friendly interface and a streamlined purchasing
-                pathway, enhancing overall user engagement. Notably, it features
-                a comprehensive dashboard for restaurant owners, offering them
-                the flexibility to effortlessly modify, add, or remove products.
-                The integration of Google Places API ensures precise location
-                services, while Cloudinary integration facilitates convenient
-                photo uploads through an intuitive drag-and-drop system. These
-                advanced features collectively guarantee an exceptional user
-                experience.
-              </p>
+              <div className={styles.topBar}></div>
 
-              <h2>Technology Stack</h2>
-              <p>
-                <b>React.JS:</b> Powers dynamic, responsive UIs with efficient
-                rendering and state management, perfect for interactive
-                applications.
-              </p>
-              <p>
-                <b>Express.JS:</b> A Node.js framework enabling quick
-                development of web and mobile apps with its minimalist approach
-                and robust features.
-              </p>
-              <p>
-                <b>JsonWebToken (JWT):</b> Secures information transfer with
-                compact, URL-safe JSON objects, ideal for authentication and
-                data exchange.
-              </p>
-              <p>
-                <b>Jest:</b> Simplifies unit testing with a zero-configuration
-                setup, known for its speed and snapshot testing capabilities.
-              </p>
-              <p>
-                <b>Cypress:</b> Revolutionizes end-to-end testing with real-time
-                execution and debugging, ensuring browser-based application
-                integrity.
-              </p>
-              <aside className={styles.buttonContainer}>
-                <button className={styles.bottomButton}>Website</button>
-                <button className={styles.bottomButton}>Github</button>
-              </aside>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+              <button
+                className={styles.closeButton}
+                onClick={() => {
+                  setIsRightContainerOpen(false);
+                }}
+              >
+                X
+              </button>
+              <div className={styles.textContainer}>
+                <h1>Gloton</h1>
+                <p>
+                  Gloton represents the pinnacle of my project experiences,
+                  inspired by the innovative Glovo platform to deliver a
+                  state-of-the-art, fully functional website. This project
+                  showcases a user-friendly interface and a streamlined
+                  purchasing pathway, enhancing overall user engagement.
+                  Notably, it features a comprehensive dashboard for restaurant
+                  owners, offering them the flexibility to effortlessly modify,
+                  add, or remove products. The integration of Google Places API
+                  ensures precise location services, while Cloudinary
+                  integration facilitates convenient photo uploads through an
+                  intuitive drag-and-drop system. These advanced features
+                  collectively guarantee an exceptional user experience.
+                </p>
+
+                <h2>Technology Stack</h2>
+                <p>
+                  <b>React.JS:</b> Powers dynamic, responsive UIs with efficient
+                  rendering and state management, perfect for interactive
+                  applications.
+                </p>
+                <p>
+                  <b>Express.JS:</b> A Node.js framework enabling quick
+                  development of web and mobile apps with its minimalist
+                  approach and robust features.
+                </p>
+                <p>
+                  <b>JsonWebToken (JWT):</b> Secures information transfer with
+                  compact, URL-safe JSON objects, ideal for authentication and
+                  data exchange.
+                </p>
+                <p>
+                  <b>Jest:</b> Simplifies unit testing with a zero-configuration
+                  setup, known for its speed and snapshot testing capabilities.
+                </p>
+                <p>
+                  <b>Cypress:</b> Revolutionizes end-to-end testing with
+                  real-time execution and debugging, ensuring browser-based
+                  application integrity.
+                </p>
+                <aside className={styles.buttonContainer}>
+                  <button className={styles.bottomButton}>Website</button>
+                  <button className={styles.bottomButton}>Github</button>
+                </aside>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </LayoutGroup>
     </motion.div>
   );
 };
