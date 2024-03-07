a1c97473e1d315320c0a148817f54effa99e4dc2
Joel Oliver <jolivermillan@gmail.com>
2024-03-07 12:55:27 +0100

    fixed css and responsive

--- src/app/components/ProjectCard/index.tsx
+++ src/app/components/ProjectCard/index.tsx
@@ -1,7 +1,9 @@
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 import styles from "./styles.module.css";
 import TechnologyCard from "../TechnologyCard";
-import React from "react";
-import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
+import rightArrowIcon from "../../../../public/static/icons/rightArrow.svg";
 import { ProjectCardProps } from "../ProjectsPage/types";
 import { TechnologyCardProps } from "../TechnologyCard/types";
 
@@ -11,7 +13,6 @@ const ProjectCard: React.FC<CombinedProps> = ({
   name,
   description,
   img,
-  stack,
   href,
   isRightContainerOpen,
   setIsRightContainerOpen,
@@ -21,51 +22,50 @@ const ProjectCard: React.FC<CombinedProps> = ({
   mongodb,
   css,
   express,
-}) => {
-  return (
-    <>
-      <motion.div className={styles.fullContainer}>
-        <a className={styles.link} href={href}>
-          <div className={styles.mainContainer}>
-            <h1 className={styles.projectTitle}>{name}</h1>
-
-            <div className={styles.imgContainer}>
-              <img className={styles.projectImg} src={img} alt="" />
-            </div>
-          </div>
-        </a>
-
-        <AnimatePresence>
-          {!isRightContainerOpen && (
-            <motion.div layout="position" className={styles.textContainer}>
-              <p>
-                {description}
-                <br /> <br />
-              </p>
-              <div className={styles.bottomContainer}>
-                <TechnologyCard
-                  react={react}
-                  javascript={javascript}
-                  html={html}
-                  mongodb={mongodb}
-                  express={express}
-                  css={css}
-                />
-                <button
-                  onClick={() => {
-                    setIsRightContainerOpen(true);
-                  }}
-                  className={styles.openMore}
-                >
-                  +
-                </button>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </motion.div>
-    </>
-  );
-};
+}) => (
+  <motion.div className={styles.fullContainer}>
+    {!isRightContainerOpen && (
+      <button
+        onClick={() => setIsRightContainerOpen(true)}
+        className={styles.openMore}
+      >
+        <img
+          src={rightArrowIcon.src}
+          alt="Open more"
+          className={styles.arrowIcon}
+        />
+      </button>
+    )}
+    <a href={href} className={styles.link}>
+      <h1 className={styles.projectTitle}>{name}</h1>
+      <div className={styles.mainContainer}>
+        <div className={styles.imgContainer}>
+          <Image
+            height={500}
+            width={500}
+            src={img}
+            alt={`${name} project thumbnail`}
+            className={styles.projectImg}
+          />
+        </div>
+      </div>
+    </a>
+    {!isRightContainerOpen && (
+      <div className={styles.textContainer}>
+        <p>{description}</p>
+        <div className={styles.bottomContainer}>
+          <TechnologyCard
+            react={react}
+            javascript={javascript}
+            html={html}
+            mongodb={mongodb}
+            express={express}
+            css={css}
+          />
+        </div>
+      </div>
+    )}
+  </motion.div>
+);
 
 export default ProjectCard;
