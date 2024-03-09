6506ac5e5e174b941c19f8cd86ee15f30b32834d
Joel Oliver <jolivermillan@gmail.com>
2024-03-09 15:37:52 +0100

    seems to work fine

--- src/app/components/ProjectCard/index.tsx
+++ src/app/components/ProjectCard/index.tsx
@@ -25,66 +25,101 @@ const ProjectCard: React.FC<CombinedProps> = ({
   setVisibleProject,
   project,
 }) => (
-  <motion.div
-    className={`${styles.fullContainer} ${
-      isRightContainerOpen && styles.closed
-    }`}
-  >
-    {!isRightContainerOpen && (
-      <button
-        onClick={() => {
-          setIsRightContainerOpen(true);
-          setVisibleProject(project);
-        }}
-        className={styles.openMore}
-      >
-        <img
-          src={rightArrowIcon.src}
-          alt="Open more"
-          className={styles.arrowIcon}
-        />
-      </button>
-    )}
-    <div
-      onClick={() => {
-        setIsRightContainerOpen(true);
-        setVisibleProject(project);
-      }}
-      className={styles.link}
-    >
-      <h1 className={styles.projectTitle}>{name}</h1>
-      <div
-        className={`${styles.mainContainer} ${
+  <>
+    {!isRightContainerOpen ? (
+      <motion.div
+        className={`${styles.fullContainer} ${
           isRightContainerOpen && styles.closed
         }`}
       >
-        <div className={styles.imgContainer}>
-          <Image
-            height={500}
-            width={500}
-            src={img}
-            alt={`${name} project thumbnail`}
-            className={styles.projectImg}
-          />
+        {!isRightContainerOpen && (
+          <button
+            onClick={() => {
+              setIsRightContainerOpen(true);
+              setVisibleProject(project);
+            }}
+            className={styles.openMore}
+          >
+            <img
+              src={rightArrowIcon.src}
+              alt="Open more"
+              className={styles.arrowIcon}
+            />
+          </button>
+        )}
+        <div
+          onClick={() => {
+            setIsRightContainerOpen(true);
+            setVisibleProject(project);
+          }}
+          className={styles.link}
+        >
+          <h1 className={styles.projectTitle}>{name}</h1>
+          <div
+            className={`${styles.mainContainer} ${
+              isRightContainerOpen && styles.closed
+            }`}
+          >
+            <div className={styles.imgContainer}>
+              <Image
+                height={500}
+                width={500}
+                src={img}
+                alt={`${name} project thumbnail`}
+                className={styles.projectImg}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    {!isRightContainerOpen && (
-      <div className={styles.textContainer}>
-        <p>{description}</p>
-        <div className={styles.bottomContainer}>
-          <TechnologyCard
-            react={react}
-            javascript={javascript}
-            html={html}
-            mongodb={mongodb}
-            express={express}
-            css={css}
-          />
+        {!isRightContainerOpen && (
+          <div className={styles.textContainer}>
+            <p>{description}</p>
+            <div className={styles.bottomContainer}>
+              <TechnologyCard
+                react={react}
+                javascript={javascript}
+                html={html}
+                mongodb={mongodb}
+                express={express}
+                css={css}
+              />
+            </div>
+          </div>
+        )}
+      </motion.div>
+    ) : (
+      <motion.div
+        className={`${styles.fullContainer} ${
+          isRightContainerOpen && styles.closed
+        }`}
+      >
+        <div
+          onClick={() => {
+            setIsRightContainerOpen(true);
+            setVisibleProject(project);
+          }}
+          className={styles.link}
+        >
+          <h1 className={styles.projectTitle}>{name}</h1>
+          <div
+            className={`${styles.mainContainer} ${
+              isRightContainerOpen && styles.closed
+            }`}
+          >
+            <div className={`${styles.imgContainer} ${styles.closed}`}>
+              <Image
+                height={500}
+                width={500}
+                src={img}
+                alt={`${name} project thumbnail`}
+                className={`${styles.projectImg} ${styles.closed}`}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      </motion.div>
     )}
-  </motion.div>
+  </>
 );
 
 export default ProjectCard;
